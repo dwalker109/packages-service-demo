86467e16ca384accffa194089ab7e79b03fe05cc
Dan Walker <dwalker109@gmail.com>
2020-02-20 13:39:58 +0000

    Adds caching and refactors sig of products service

--- server/src/domain/products.service.ts
+++ server/src/domain/products.service.ts
@@ -10,17 +10,35 @@ interface ApiProduct {
   usdPrice: number;
 }
 
-const getProduct = async (id: string): Promise<Response> => {
+type ApiProductCache = Map<string, ApiProduct>
+
+/**
+ * Define a cache and clear it on an interval
+ */
+const cache: ApiProductCache = new Map();
+setInterval(() => cache.clear(), 60 * 1000);
+
+/**
+ * Retrieve and cache a product
+ */
+const getProduct = async (id: string): Promise<ApiProduct> => {
+  let product = cache.get(id);
+  if (product) return product;
+
   const headers = new Headers({ Authorization: `Basic ${authToken}` });
-  return fetch(`${url}/${id}`, { headers });
+  const response = await fetch(`${url}/${id}`, { headers });
+  product = await response.json();
+  cache.set(id, product);
+
+  return product;
 };
 
+/**
+ * Retrieve multiple products
+ */
 const getProducts = async (ids: string[]): Promise<ApiProduct[]> =>
   Promise.all(
-    ids.map(async id => {
-      const response = await getProduct(id);
-      return response.json();
-    })
+    ids.map(async id => getProduct(id))
   );
 
 export { getProduct, getProducts };
