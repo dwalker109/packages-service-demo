510cafff7ce30c81447e0e30f0e89d5f80748213
Dan Walker <dan@dans-macbook.home>
2020-02-23 22:18:00 +0000

    Adds confirm prompt on currency switch

--- app/src/components/currency/CurrencyWidget.tsx
+++ app/src/components/currency/CurrencyWidget.tsx
@@ -17,8 +17,12 @@ const CurrencyWidget: FC = () => {
     <div className="CurrencyWidget-main">
       <select
         value={active}
-        onChange={(e): PayloadAction<string> =>
-          dispatch(clearBasket()) && dispatch(setActive(e.target.value))
+        onChange={(e): PayloadAction<string> | false =>
+          window.confirm(
+            "Switching currency will empty your basket - continue?"
+          ) &&
+          dispatch(clearBasket()) &&
+          dispatch(setActive(e.target.value))
         }
       >
         {available.map(currency => (
